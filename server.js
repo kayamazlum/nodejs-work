926bce9d227bce6d023f0394d2463f8c67d5ca87
kayamazlum <mazlum09kaya@gmail.com>
2024-11-02 23:23:32 +0300

    fs modulu ile req atılan urli text dosyasına kaydetmek

--- server.js
+++ server.js
@@ -1,17 +1,24 @@
 // request -> server -> response(+/-)
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-  switch (req.url) {
-    case "/":
-      res.end("ana sayfa");
-      break;
-    case "/about":
-      res.end("hakkımda");
-      break;
-    default:
-      res.end("404 - Böyle bir sayfa yok");
-  }
+  fs.appendFile("log.txt", req.url, (err, html) => {
+    if (err) {
+      throw new Error("hataaaaaaaaaaaa");
+    } else {
+      switch (req.url) {
+        case "/":
+          res.end("ana sayfa");
+          break;
+        case "/about":
+          res.end("hakkımda");
+          break;
+        default:
+          res.end("404 - Böyle bir sayfa yok");
+      }
+    }
+  });
 });
 
 server.listen(8000, () => {
